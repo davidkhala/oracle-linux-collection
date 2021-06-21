3d14300ad1889af950695eb2d7fc8253d11902ee
David Liu <david.yx.liu@oracle.com>
2021-06-21 02:35:13 -0400

    Delete setup.sh

--- mysql/setup.sh
+++
@@ -1,8 +0,0 @@
-install(){
-  sudo yum install -y https://dev.mysql.com/get/mysql80-community-release-el8-1.noarch.rpm
-  sudo yum install -y mysql
-}
-Shell(){
-  sudo yum install -y mysql-shell
-}
-$@
