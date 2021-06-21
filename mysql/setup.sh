f10983c50534ce6a30fdb277b1320914a401c0c6
David Liu <david.yx.liu@oracle.com>
2021-06-20 22:58:02 -0400

    Update setup.sh

--- mysql/setup.sh
+++ mysql/setup.sh
@@ -1,5 +1,8 @@
 install(){
   sudo yum install -y https://dev.mysql.com/get/mysql80-community-release-el8-1.noarch.rpm
-  sudo yum install mysql
+  sudo yum install -y mysql
+}
+Shell(){
+  sudo yum install -y mysql-shell
 }
 $@
