a0668b1e845e0f073c6027e5d02b54b67170e29b
David Liu <david.yx.liu@oracle.com>
2021-09-23 00:05:09 -0400

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,4 +1,8 @@
-
+install(){
+    sudo yum erase podman buildah
+    sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+    sudo dnf install -y docker-ce
+}
 OCR(){
     # login to Oracle Container Registry
     docker login container-registry.oracle.com
