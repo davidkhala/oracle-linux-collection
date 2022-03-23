a9ef642096f3a7e953bda529e525dd168156e60b
David Liu <david.yx.liu@oracle.com>
2022-03-22 23:57:46 -0400

    Update docker.sh

--- 7/docker.sh
+++ 7/docker.sh
@@ -1,6 +1,10 @@
 install(){
     sudo dnf install -y docker-ce
 }
+rootless(){
+    export FORCE_ROOTLESS_INSTALL=1
+    curl -fsSL https://get.docker.com/rootless | sh
+}
 uninstall(){
     sudo dnf erase -y docker-ce
 }
