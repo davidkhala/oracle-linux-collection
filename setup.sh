dd277973cb2e6082e03c99a59178e79d83e6c962
David Liu <david.yx.liu@oracle.com>
2022-07-21 13:48:03 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 add-repo(){
   yum-config-manager --add-repo http://yum.oracle.com/repo/OracleLinux/OL8/3/baseos/base/x86_64
 }
+oci(){
+  sudo dnf -y install oraclelinux-developer-release-el8
+  sudo dnf -y install python36-oci-cli
+}
 $@
   
