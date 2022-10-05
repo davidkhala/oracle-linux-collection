1a24bc089889b0003456549f8d498a0e4d877f7b
David Liu <david.yx.liu@oracle.com>
2022-10-05 22:55:21 +0800

    Update instant-client.sh

--- instant-client.sh
+++ instant-client.sh
@@ -5,4 +5,10 @@ install-light(){
 #  TODO verify basiclite or basiclight?
   sudo dnf install oracle-instantclient-basiclite
 }
+install(){
+  sudo dnf install oracle-instantclient-basic
+}
+install-sqlplus(){
+  sudo dnf install oracle-instantclient-sqlplus
+}
 $@
