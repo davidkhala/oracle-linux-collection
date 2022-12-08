d1d34eb1b9ffc8dbaaa24699b4050d69ee977acb
David Liu <david.yx.liu@oracle.com>
2022-12-08 16:05:14 +0800

    Create oracledb.sh

---
+++ 7/oracledb.sh
@@ -0,0 +1,5 @@
+install-instant-client(){
+  wget https://download.oracle.com/otn_software/linux/instantclient/218000/oracle-instantclient-basiclite-21.8.0.0.0-1.x86_64.rpm
+  sudo yum install -y oracle-instantclient-basiclite-21.8.0.0.0-1.x86_64.rpm
+}
+$@
