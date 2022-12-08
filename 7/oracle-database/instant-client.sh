20b2676afed7658704843b4e12f9545f1de76329
David Liu <david.yx.liu@oracle.com>
2022-12-08 16:09:09 +0800

    Update and rename 7/oracledb.sh to 7/oracle-database/instant-client.sh

--- 7/oracle-database/instant-client.sh
+++ 7/oracle-database/instant-client.sh
@@ -1,4 +1,4 @@
-install-instant-client(){
+install(){
   wget https://download.oracle.com/otn_software/linux/instantclient/218000/oracle-instantclient-basiclite-21.8.0.0.0-1.x86_64.rpm
   sudo yum install -y oracle-instantclient-basiclite-21.8.0.0.0-1.x86_64.rpm
 }
