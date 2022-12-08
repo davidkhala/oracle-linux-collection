6116a12e504dd955506018733df195d2de0cda92
David Liu <david.yx.liu@oracle.com>
2022-12-08 16:11:23 +0800

    Update instant-client.sh

--- 7/oracle-database/instant-client.sh
+++ 7/oracle-database/instant-client.sh
@@ -1,5 +1,5 @@
 install(){
-  wget https://download.oracle.com/otn_software/linux/instantclient/218000/oracle-instantclient-basiclite-21.8.0.0.0-1.x86_64.rpm
-  sudo yum install -y oracle-instantclient-basiclite-21.8.0.0.0-1.x86_64.rpm
+  sudo yum install -y oracle-instantclient-release-el7
+  sudo yum install -y oracle-instantclient-basiclite
 }
 $@
