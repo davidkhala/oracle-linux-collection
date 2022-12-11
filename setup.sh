094a60da1ad82ba7dbdcedb369b7be336ffa8b90
David Liu <david.yx.liu@oracle.com>
2022-12-11 19:52:45 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,4 @@
-repo() {
-  yum-config-manager --add-repo http://yum.oracle.com/repo/OracleLinux/OL8/3/baseos/base/x86_64
-}
+
 dev() {
   sudo dnf install -y oraclelinux-developer-release-el8
 
@@ -11,7 +9,7 @@ xclip() {
 }
 desktop(){
   sudo dnf update
-  sudo dnf groupinstall "Server with GUI"
+  sudo dnf groupinstall -y "Server with GUI"
   systemctl set-default graphical.target
   ln -sf /lib/systemd/system/runlevel5.target /etc/systemd/system/default.target
   sudo reboot
