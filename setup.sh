6e7f8e00ba1a7ab2e857fd9112b11671aca34b07
David Liu <david.yx.liu@oracle.com>
2022-12-11 19:49:12 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,5 +9,12 @@ xclip() {
   sudo dnf install -y oracle-epel-release-el8
   sudo dnf install -y xclip
 }
+desktop(){
+  sudo dnf update
+  sudo dnf groupinstall "Server with GUI"
+  systemctl set-default graphical.target
+  ln -sf /lib/systemd/system/runlevel5.target /etc/systemd/system/default.target
+  sudo reboot
+}
 
 $@
