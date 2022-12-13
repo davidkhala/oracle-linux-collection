8eeb5c5a5ba98c2d2c769f13b291bcd863c47115
David Liu <david.yx.liu@oracle.com>
2022-12-13 18:22:10 +0800

    Update install.sh

--- 9/install.sh
+++ 9/install.sh
@@ -1,4 +1,7 @@
 epel(){
   sudo dnf config-manager --add-repo=http://yum.oracle.com/repo/OracleLinux/OL9/developer/EPEL/x86_64
 }
+dev-epel(){
+  sudo dnf config-manager --enable ol9_developer_EPEL
+}
 $@
